cb66650c51ea2efcb3ac2ffdc8574d02a2b561fb
ThaMoreira <thamoreira@icloud.com>
2025-07-10 13:33:42 -0300

    feat: acept local request

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}!`,
